5dc2fa2a97e974b86901b9fe8bf621cd69f21a20
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-01 00:23:17 -0300

    📚feat: Add Pagination Concern and Include it in ApplicationController
    This commit introduces a new module named Pagination in the concerns folder. This module includes several helper methods to handle pagination logic, including:
    
    1. page_num: to determine the current page number.
    2. per_page: to determine how many records to display per page.
    3. all?: to check if all records should be displayed.
    4. paginate_offset: to calculate the starting point of the records for the current page.
    5. order_by and order_direction: to sort the records.
    6. paginate: to apply the limit, offset, and order to the records.
    
    Additionally, Pagination is included in ApplicationController to be available to all controllers.

---
+++ app/controllers/concerns/pagination.rb
@@ -0,0 +1,43 @@
+module Pagination
+  extend ActiveSupport::Concern
+
+  DEFAULT_PER_PAGE = 25.freeze
+  ALL_PER_PAGE = "all".freeze
+
+  def page_num
+    params.fetch(:page, 1).to_i
+  end
+
+  def per_page
+    params.fetch(:per_page, DEFAULT_PER_PAGE).to_i
+  end
+
+  def all?
+    params[:per_page] == ALL_PER_PAGE
+  end
+
+  def paginate_offset
+    (page_num - 1) * per_page
+  end
+
+  def order_by
+    params.fetch(:order_by, :id)
+  end
+
+  def order_direction
+    params.fetch(:order_direction, :asc)
+  end
+
+  def paginate
+    ->(records) do
+      records = records.limit(per_page).offset(paginate_offset) unless all?
+      records.order("#{order_by}": order_direction)
+    end
+  end
+
+  private
+
+  def model
+    controller_name.classify.constantize
+  end
+end
