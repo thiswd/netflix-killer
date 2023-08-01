bd1158e3038c299a71360c94d2c55fb2a75ca776
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-01 02:00:17 -0300

    fix: typo create

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :movies, only: %i[index]
 
   resources :users, only: [] do
-    resources :rentals, only: [:index, :post]
+    resources :rentals, only: [:index, :create]
     get "recomendations", on: :member
   end
 end
